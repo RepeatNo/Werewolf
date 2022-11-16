2510ff99145fa2b13f3f4e4ca72d8a3f00cbbbde
Alexander Dick <alexander.dick03@gmail.com>
2022-11-16 17:43:55 +0100

    impl ConfigManager

--- src/main/java/at/ingameengine/utils/ConfigManager.java
+++ src/main/java/at/ingameengine/utils/ConfigManager.java
@@ -1,14 +1,50 @@
 package at.ingameengine.utils;
 
 import at.ingameengine.werewolf.Werewolf;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ConfigManager {
-    FileConfiguration config;
-    Werewolf plugin;
+    static final String directory = "plugins//Werewolf//";
 
-    public ConfigManager(Werewolf plugin) {
-        this.config = plugin.getConfig();
+    Werewolf plugin;
+    YamlConfiguration config = new YamlConfiguration();
+    File file;
+
+    public ConfigManager(Werewolf plugin, String fileName) {
         this.plugin = plugin;
+        this.file = new File(directory + "//" + fileName);
+        loadConfig();
+    }
+
+    public void loadConfig() {
+        try {
+            if (!file.exists()) {
+                new File(directory).mkdirs();
+                file.createNewFile();
+            }
+            config.load(file);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveConfig() {
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setConfig(String configName, String value) {
+        config.set(configName, value);
+    }
+
+    public String readConfigString(String configName) {
+        return config.getString(configName);
     }
 }
