8b8cb08ab665f87b78ac668fc8c1eadbbcdccdf7
Alexander Dick <alexander.dick03@gmail.com>
2022-11-19 19:44:00 +0100

    removed redundancy

--- src/main/java/at/ingameengine/utils/InventoryBuilder.java
+++ src/main/java/at/ingameengine/utils/InventoryBuilder.java
@@ -1,19 +1,16 @@
 package at.ingameengine.utils;
 
 import at.ingameengine.werewolf.Werewolf;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.javatuples.Pair;
 
-import java.util.Dictionary;
 import java.util.HashMap;
 
 public class InventoryBuilder {
 
-    private Werewolf plugin;
+    private final Werewolf plugin;
 
     public InventoryBuilder(Werewolf plugin) {
         this.plugin = plugin;
@@ -22,20 +19,20 @@ public class InventoryBuilder {
     public Inventory setupInventory() {
         HashMap<Pair<Integer, Integer>, ItemStack> items = new HashMap<Pair<Integer, Integer>, ItemStack>();
         items.put(
-                new Pair<Integer, Integer>(1, 1),
+                new Pair<>(1, 1),
                 new ItemManager().getHead("§eProfile §6» §c" + plugin.getGameProfile().getName(),
                         plugin.getSkullManager().readString("setup.profile"), null));
-        items.put(new Pair<Integer, Integer>(3, 3), new ItemManager().getItem(Material.COMPASS, 1, "§eBasics"));
-        items.put(new Pair<Integer, Integer>(3, 5), new ItemManager().getItem(Material.MAGMA_CREAM, 1, "§eSpawns"));
-        items.put(new Pair<Integer, Integer>(3, 7), new ItemManager().getItem(Material.BOOK, 1, "§eGame Profiles"));
+        items.put(new Pair<>(3, 3), new ItemManager().getItem(Material.COMPASS, 1, "§eBasics"));
+        items.put(new Pair<>(3, 5), new ItemManager().getItem(Material.MAGMA_CREAM, 1, "§eSpawns"));
+        items.put(new Pair<>(3, 7), new ItemManager().getItem(Material.BOOK, 1, "§eGame Profiles"));
 
         return buildInventory(9 * 5, "§eSetup", items);
     }
 
     public Inventory GameProfileInventory() {
         HashMap<Pair<Integer, Integer>, ItemStack> items = new HashMap<Pair<Integer, Integer>, ItemStack>();
-        items.put(new Pair<Integer, Integer>(3, 3), new ItemManager().getItem(Material.COMPASS, 1, "§eBasics"));
-        items.put(new Pair<Integer, Integer>(1, 1), new ItemManager().getHead(
+        items.put(new Pair<>(3, 3), new ItemManager().getItem(Material.COMPASS, 1, "§eBasics"));
+        items.put(new Pair<>(1, 1), new ItemManager().getHead(
                 "§eProfile §6» §c" + plugin.getGameProfile().getName(),
                 plugin.getSkullManager().readString("setup.profile"),
                 null));
@@ -43,7 +40,7 @@ public class InventoryBuilder {
         int number = 5;
         for (int row = 2; row < 4; row++) {
             for (int column = 2; column < 9; column++) {
-                items.put(new Pair<Integer, Integer>(row, column),
+                items.put(new Pair<>(row, column),
                         new ItemManager().getItem(Material.BOOK,
                                 number,
                                 "§6#" + number));
